8a064ebf79c5e473a721bd849a6883a612702813
Jim Snodgrass <snodgrass23@gmail.com>
2020-03-31 18:32:31 -0400

    build production

---
+++ precache-manifest.4431e94ea8bb74b7dd7b5923574e6bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73447580ea57606806414f909e62e1bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35bfef5b8f603363bf15",
+    "url": "/static/css/main.0145b904.chunk.css"
+  },
+  {
+    "revision": "773ef4ad2369f1829d8b",
+    "url": "/static/js/2.cfce7918.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cfce7918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35bfef5b8f603363bf15",
+    "url": "/static/js/main.a1383d84.chunk.js"
+  },
+  {
+    "revision": "c081a61104c9ff94d2d4",
+    "url": "/static/js/runtime-main.f16a4c84.js"
+  }
+]);
--- src/Player.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-const Player = () => {
-  return (
-    <div>Test</div>
-  );
-}
-
-export default Player;
